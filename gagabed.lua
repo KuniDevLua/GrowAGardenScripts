8ace0eadefca8c6a5a19b1096937e4df934aa3e9
KuniDevLua <profkunin@gmail.com>
2025-07-22 00:09:08 +0800

    Create gagabed.lua

---
+++ gagabed.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/GrowAGarden/refs/heads/main/Petv1"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/vkKxbY9cAq/0", true))()
