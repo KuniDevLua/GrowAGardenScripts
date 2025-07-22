1e1913f1e4ebef59585c9addca0c010d8d176fed
KuniDevLua <profkunin@gmail.com>
2025-07-23 06:26:11 +0800

    Update EggRandomizer.lua

--- EggRandomizer.lua
+++ EggRandomizer.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/GrowAGarden/refs/heads/main/FullFeaturesObfuscasted"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/GrowAGarden/refs/heads/main/FullFeaturesWithLoadingOBS"))()
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/7qt4cHtvvlU1gLYG/raw", true))()
