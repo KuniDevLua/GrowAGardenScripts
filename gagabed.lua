4309052bcad42fc3176b6d0a47dcb23e12f15f7f
KuniDevLua <profkunin@gmail.com>
2025-07-22 18:02:37 +0800

    Update gagabed.lua

--- gagabed.lua
+++ gagabed.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/GrowAGarden/refs/heads/main/Petv1"))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/vkKxbY9cAq/0", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/8M2THuGkv5OD4zIc/raw", true))()
