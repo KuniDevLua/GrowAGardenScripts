e60b2e24051757681271a2ecd608b3e0b34beac3
KuniDevLua <profkunin@gmail.com>
2025-07-24 22:30:11 +0800

    Update and rename PetSpawner.lua to PetSpawnerGAG.lua

--- PetSpawner.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/GrowAGardenScripts/refs/heads/main/Egorhits"))()
---
+++ PetSpawnerGAG.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KuniDevLua/GrowAGardenScripts/refs/heads/main/etona"))
